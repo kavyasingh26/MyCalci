952d404658d08b92106905d958ab9d9434b011cf
Pragya Vats <133896980+Pragyavats09@users.noreply.github.com>
2023-05-17 23:58:56 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/mycalci/MainActivity.java
+++ app/src/main/java/com/example/mycalci/MainActivity.java
@@ -2,18 +2,37 @@ package com.example.mycalci;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import android.os.Bundlachae;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    TextView workingsTV;
+    TextView resultsTV;
+    String workings ="";
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    private void initTextView()
+    {
+        workingsTV =(TextView)findViewById(R.id.workingsTextView);
+        resultsTV = (TextView)findViewById(R.id.resultTextView);
+    }  
+    private void setworkings(String givenValue)
+    {
+        workings = workings + givenValue;
+        workingsTV.setText(workings);
+    }    
+        
 
     public void clearOnclick(View view) {
+        workings TV .setText("");
+        workings = "";
+        resultsTV.setText(""); 
+        
     }
 
     public void FourOnclick(View view) {
@@ -23,27 +42,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void nineOnclick(View view) {
+        setWorkings("9");
     }
 
     public void eightOnclick(View view) {
+        setWorkings("8");
     }
 
     public void sevenOnclick(View view) {
+        setWorkings("7");
     }
 
     public void divisionOnclick(View view) {
+        setWorkings("/");
     }
 
     public void powerOfOnclick(View view) {
+        setWorkings("^");
     }
 
     public void bracketOnclick(View view) {
     }
 
     public void FiveOnclick(View view) {
+        setWorkings("5");
     }
 
     public void SixOnclick(View view) {
+       setWorkings("6"); 
     }
 
     public void Onclick(View view) {
@@ -59,14 +85,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void ThreeOnclick(View view) {
+        setWorkings("3");
     }
 
     public void PlusOnclick(View view) {
+        setWorkings("+");
     }
 
     public void ZeroOnclick(View view) {
+        setWorkings("0");
     }
 
     public void EqualOnclick(View view) {
     }
+    
+    public void decimalOnClick(View view)
+    {
+        setWorkings(".");
+    }    
 }
