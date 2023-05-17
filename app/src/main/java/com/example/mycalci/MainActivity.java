abc47be5c6c17bdf52880afe4d09fec83e27c72b
Kavya343 <kavya343ks@gmail.com>
2023-05-17 22:08:54 +0530

    this is my 2nd commit

---
+++ app/src/main/java/com/example/mycalci/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.mycalci;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clearOnclick(View view) {
+    }
+
+    public void FourOnclick(View view) {
+    }
+
+    public void MultiplicationOnclick(View view) {
+    }
+
+    public void nineOnclick(View view) {
+    }
+
+    public void eightOnclick(View view) {
+    }
+
+    public void sevenOnclick(View view) {
+    }
+
+    public void divisionOnclick(View view) {
+    }
+
+    public void powerOfOnclick(View view) {
+    }
+
+    public void bracketOnclick(View view) {
+    }
+
+    public void FiveOnclick(View view) {
+    }
+
+    public void SixOnclick(View view) {
+    }
+
+    public void Onclick(View view) {
+    }
+
+    public void MinusOnclick(View view) {
+    }
+
+    public void OneOnclick(View view) {
+    }
+
+    public void TwoOnclick(View view) {
+    }
+
+    public void ThreeOnclick(View view) {
+    }
+
+    public void PlusOnclick(View view) {
+    }
+
+    public void ZeroOnclick(View view) {
+    }
+
+    public void EqualOnclick(View view) {
+    }
+}
