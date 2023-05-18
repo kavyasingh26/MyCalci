56e69fcf75f388eec6c49e9b7cc539a1e3e2a977
dtlnyx <133896707+dtlnyx@users.noreply.github.com>
2023-05-18 08:00:08 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/mycalci/MainActivity.java
+++ app/src/main/java/com/example/mycalci/MainActivity.java
@@ -115,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
          
     }
     
-    public  void decimalOnClick(View view)
+    public  void decimalOnClick(View view);
     {
         setWorkings(".");
     }    
